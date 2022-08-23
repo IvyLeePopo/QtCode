164ee115c6f3dac4450ed7c657c8f1d936f60ba3
popo <lip@corp.4dstc.com>
2022-08-23 10:34:19 +0800

    add setting

---
+++ QtSetting/qt_setting.cpp
@@ -0,0 +1,81 @@
+﻿#include "pch.h"
+#include "qt_setting.h"
+
+#include "defs.h"
+
+QtSetting::QtSetting()
+{
+#ifdef Q_WS_WIN
+	m_pSetting = new QSettings(REG_SHOETECH, QSettings::NativeFormat);
+#else
+	m_pSetting = new QSettings(REG_SHOETECH, QSettings::IniFormat);
+#endif
+}
+
+QtSetting::~QtSetting()
+{
+	if (m_pSetting)
+		delete m_pSetting;
+}
+
+QVariant QtSetting::readSetting(const QString& group, const QString& key, const QVariant& defaultValue)
+{
+	m_pSetting->beginGroup(group);
+	QVariant variantValue = m_pSetting->value(key, defaultValue);
+	m_pSetting->endGroup();
+
+	return variantValue;
+}
+
+QVariant QtSetting::readSetting(const QString& key, const QVariant& defaultValue /*= QVariant()*/)
+{
+	return m_pSetting->value(key, defaultValue);
+}
+
+void QtSetting::writeSetting(const QString& group, const QString& key, const QVariant& value)
+{
+	m_pSetting->beginGroup(group);
+	m_pSetting->setValue(key, value);
+	m_pSetting->endGroup();
+}
+
+void QtSetting::writeSetting(const QString& key, const QVariant& value)
+{
+	m_pSetting->setValue(key, value);
+}
+
+void QtSetting::removeSetting(const QString& group, const QString& key)
+{
+	m_pSetting->beginGroup(group);
+	m_pSetting->remove(key);
+	m_pSetting->endGroup();
+}
+
+void QtSetting::removeSetting(const QString& key)
+{
+	m_pSetting->remove(key);
+}
+
+bool QtSetting::contains(const QString& group, const QString& key)
+{
+	m_pSetting->beginGroup(group);
+	bool bContain = m_pSetting->contains(key);
+	m_pSetting->endGroup();
+
+	return bContain;
+}
+
+bool QtSetting::contains(const QString& key)
+{
+	return m_pSetting->contains(key);
+}
+
+void QtSetting::setValue(const QString &key, const QVariant &value)
+{
+	m_pSetting->setValue(key, value);
+}
+
+QVariant QtSetting::value(const QString &key, const QVariant &defaultValue /*= QVariant()*/) const
+{
+	return m_pSetting->value(key, defaultValue);
+}
---
+++ QtSetting/qt_setting.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include <QSettings>
+
+class QtSetting
+{
+public:
+	explicit QtSetting();
+	~QtSetting();
+
+public:
+	QVariant readSetting(const QString& group, const QString& key, const QVariant& defaultValue);
+	QVariant readSetting(const QString& key, const QVariant& defaultValue = QVariant());
+
+	void writeSetting(const QString& group, const QString& key, const QVariant& value);
+	void writeSetting(const QString& key, const QVariant& value);
+
+	void removeSetting(const QString& group, const QString& key);
+	void removeSetting(const QString& key);
+
+	bool contains(const QString& group, const QString& key);
+	bool contains(const QString& key);
+
+	void setValue(const QString &key, const QVariant &value);
+	QVariant value(const QString &key, const QVariant &defaultValue = QVariant()) const;
+
+private:
+	QSettings* m_pSetting;
+};
